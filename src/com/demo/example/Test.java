babdafa6d92aaa4b7120c392d922659e28d42775
BenStone007 <floydcr007@gmail.com>
2025-04-03 14:54:34 +0530

    First commit

---
+++ src/com/demo/example/Test.java
@@ -0,0 +1,14 @@
+package com.demo.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+	public static WebDriver driver = new ChromeDriver();
+
+	public static void main(String[] args) {
+
+		System.out.println("Hi am john Welcome to the Automation World");
+
+	}
+}
