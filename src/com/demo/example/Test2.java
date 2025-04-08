f66b0854c68c2750851d43771d0519b1b10e98a7
Harryzen007 <harringtonkit571@gmail.com>
2025-04-08 11:37:05 +0530

    Commiting again after the pull request

--- src/com/demo/example/Test2.java
+++ src/com/demo/example/Test2.java
@@ -1,9 +1,15 @@
 package com.demo.example;
 
 public class Test2 {
-
+		
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Test two from harry");
+		
+		String name="Harry";
+		if (name.equals(name)) {
+			System.out.println("Yes the name contains the text");
+		}
+		
+//		System.out.println("Hello Test two from harry");
 	}
 }
